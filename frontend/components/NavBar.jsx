574ecfba7c14be960bd22725e8e4f0b80ac2f0d6
HShaw215 <hu.shaw215@gmail.com>
2023-07-06 15:52:37 -0400

    Configured Navbar to be redirecting buttons

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -6,8 +6,9 @@ const NavBar = () => {
     return (
         <>
         <nav>
-            <a href='https://github.com/HShaw215'>Projects</a>
-            <a href='hu.shaw215@gmail.com'>Email</a>
+            <button onClick = {() => location.href='https://github.com/HShaw215'}>Projects</button>
+            <button onClick = {() => location.href='https://www.linkedin.com/in/hshaw215/'} >LinkedIn</button>
+            <button onClick = {() => location.href='hu.shaw215@gmail.com'}>Email</button>
         </nav>
         </>
     )
