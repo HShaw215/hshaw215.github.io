f7799a12700739494d952b725e5387c12d256033
HShaw215 <hu.shaw215@gmail.com>
2023-07-08 19:57:26 -0400

    adjusted horizontal layout

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -8,18 +8,21 @@ const Main = () => {
     return (
         <div class="main">
             <NavBar />
-            <div class="pic">
-                <img src={hunter} class="headshot"></img>
+            <div class='text'>
+                <div class="pic">
+                    <img src={hunter} class="headshot"></img>
+                </div>
+                <div class="body">
+                    <p>
+                        Hello, my name is Hunter Shaw. I am a software engineer currently working at ReKuberate, a Kubernetes cluster visualizer
+                        and health monitor. My responsibilities are across the entire stack but I am passionate about backend development.
+                    </p>
+                    <p>
+                        Outside of coding I am passionate about hockey, reading, skiing, and DJing.
+                    </p>
+                </div>
             </div>
-            <div class="body">
-                <p>
-                    Hello, my name is Hunter Shaw. I am a software engineer currently working at ReKuberate, a Kubernetes cluster visualizer
-                    and health monitor. My responsibilities are across the entire stack but I am passionate about backend development.
-                </p>
-                <p>
-                    Outside of coding I am passionate about hockey, reading, skiing, and DJing.
-                </p>
-            </div>
+            
         </div> 
     )
 }
