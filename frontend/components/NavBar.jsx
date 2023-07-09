5e746776e84624930c723d4df8484d7e7d204011
HShaw215 <hu.shaw215@gmail.com>
2023-07-08 22:46:54 -0400

    fixing nav bar and bio spacing

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
             <button onClick = {() => location.href='https://www.linkedin.com/in/hshaw215/'} >LinkedIn</button>
             <button onClick = {() => location.href='mailto:hu.shaw215@gmail.com'}>Email</button> */}
             <header><strong>Hunter Shaw</strong></header>
-            <div>
+            <div class='links'>
                 <a href='https://github.com/HShaw215'>Projects</a>
                 <a href='https://www.linkedin.com/in/hshaw215/'>LinkedIn</a>
                 <a href='mailto:hu.shaw215@gmail.com'>Email</a>
