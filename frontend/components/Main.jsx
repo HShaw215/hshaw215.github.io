9a47275fd5145d6b369567ebafc4b09bb941e88e
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 16:42:56 -0400

    fixed bio wording for flow

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -14,18 +14,18 @@ const Main = () => {
                     <p>
                         Hello, I'm Hunter. I am a software engineer living in New York. Currently I am working at 
                         <a class='project' href='https://rekuberate.org'> ReKuberate</a>
-                        , a Kubernetes cluster visualizer
-                        and health monitor. My responsibilities are across the entire stack but my true passion is in
+                        , an Open Source Kubernetes cluster monitoring tool with visualization capabilities.
+                        My responsibilities are across the entire stack but my true passion is in
                         backend development.
                     </p>
                     <p>
-                        My most recent project that I am working on is 
-                        <a class='project' href='https://github.com/ConquestIO/Conquest'> Conquest</a>
-                        , a test tracking tool used to promote test driven development across teams.
+                        My most recent project that I am still currently working on is 
+                        <a class='project' href='https://conquest-7ffadf4c2da1.herokuapp.com/'> Conquest</a>
+                        , a testing suite tracking tool used to promote test driven development across teams.
                     </p>
                     <p>
                         Outside of coding I am passionate about hockey, reading, skiing, and DJing. If you want to talk more about
-                        coding or even any of my hobbies, feel free to reach out to me via email or on LinkedIn.
+                        coding or any of my hobbies, feel free to reach out to me via email or on LinkedIn.
                     </p>
                 </div>
             </div>
