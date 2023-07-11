8003d5bb2ce545369a1ceb08bef1648846083ce9
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 19:57:57 -0400

    converted to hashrouter for deployment issue

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ReactDOM } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import Main from './frontend/components/Main.jsx'
 import NavBar from './frontend/components/NavBar.jsx';
 
-const domNode = document.getElementById('container')
-const root = createRoot(domNode)
-root.render(
-  <div>
-    <NavBar />
-    <Main />
-  </div>
-)
-    
+createRoot(document.getElementById('container')).render(
+    <HashRouter>
+      <NavBar />
+      <Main />
+    </HashRouter>
+  );
