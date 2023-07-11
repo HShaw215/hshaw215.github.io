e56d68d4e86f15503b1b3e80da8f051f556f0e3d
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 19:48:01 -0400

    removed react router

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ReactDOM } from 'react-dom/client';
 import Main from './frontend/components/Main.jsx'
 import NavBar from './frontend/components/NavBar.jsx';
 
-createRoot(document.getElementById('container')).render(
-      <>
-      <NavBar />
-      <Main />
-      </>
-  );
+const domNode = document.getElementById('container')
+const root = createRoot(domNode)
+root.render(
+  <div>
+    <NavBar />
+    <Main />
+  </div>
+)
+    
