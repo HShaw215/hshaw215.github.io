5741773ac701c7ad0f017167893e2c7e6100c7b2
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 16:30:56 -0400

    added Conquest app to bio

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -12,15 +12,16 @@ const Main = () => {
                 <img src={hunter} class="pic" alt="Hunter Shaw"></img>
                 <div class="bio">
                     <p>
-                        Hello, my name is Hunter Shaw. I am a software engineer currently working at 
-                        <a id='rekuberate' href='https://rekuberate.org'> ReKuberate</a>
+                        Hello, I'm Hunter. I am a software engineer living in New York. Currently I am working at 
+                        <a class='project' href='https://rekuberate.org'> ReKuberate</a>
                         , a Kubernetes cluster visualizer
                         and health monitor. My responsibilities are across the entire stack but my true passion is in
                         backend development.
                     </p>
                     <p>
-                        A list of my previous work can be found by going to the Projects link above. All of my portfolio projects
-                        are starred at the top of my Github.
+                        My most recent project that I am working on is 
+                        <a class='project' href='https://github.com/ConquestIO/Conquest'> Conquest</a>
+                        , a test tracking tool used to promote test driven development across teams.
                     </p>
                     <p>
                         Outside of coding I am passionate about hockey, reading, skiing, and DJing. If you want to talk more about
--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -8,10 +8,10 @@ const NavBar = () => {
         <div class='nav'>
             <a class='name' href='./index.html'>Hunter Shaw</a>
             <div class='menu'>
-                <a class='menuItem' href='https://github.com/HShaw215'>Projects</a>
+                <a class='menuItem' href='https://github.com/HShaw215'>Portfolio</a>
                 <a class='menuItem' href='https://www.linkedin.com/in/hshaw215/'>LinkedIn</a>
-                <a class='menuItem' href='mailto:hu.shaw215@gmail.com'>Email</a>
                 <a class='menuItem' href='https://youtu.be/EvpbJDMiep4'>Talks</a>
+                <a class='menuItem' href='mailto:hu.shaw215@gmail.com'>Contact</a>
             </div>
         </div>
         </>
