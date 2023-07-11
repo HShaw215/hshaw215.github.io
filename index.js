e4803c77b51be0cac4d79b4916cfb419f604c495
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 19:38:43 -0400

    removed browser router to help compiling

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import Main from './frontend/components/Main.jsx'
 import NavBar from './frontend/components/NavBar.jsx';
 
 createRoot(document.getElementById('container')).render(
-    <BrowserRouter>
+      <>
       <NavBar />
       <Main />
-    </BrowserRouter>
+      </>
   );
