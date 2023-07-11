2be2cb37ae1187eab4f3a2ccf88b713282d643ee
HShaw215 <hu.shaw215@gmail.com>
2023-07-11 15:46:35 -0400

    added background and fixing layout

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -6,17 +6,13 @@ const NavBar = () => {
     return (
         <>
         <div class='nav'>
-            {/* <button onClick = {() => location.href='https://github.com/HShaw215'}>Projects</button>
-            <button onClick = {() => location.href='https://www.linkedin.com/in/hshaw215/'} >LinkedIn</button>
-            <button onClick = {() => location.href='mailto:hu.shaw215@gmail.com'}>Email</button> */}
             <a class='name' href='./index.html'>Hunter Shaw</a>
-            <div class='links'>
-                <a href='https://github.com/HShaw215'>Projects</a>
-                <a href='https://www.linkedin.com/in/hshaw215/'>LinkedIn</a>
-                <a href='mailto:hu.shaw215@gmail.com'>Email</a>
-                <a href='https://youtu.be/EvpbJDMiep4'>Tech Talk</a>
+            <div class='menu'>
+                <a class='menuItem' href='https://github.com/HShaw215'>Projects</a>
+                <a class='menuItem' href='https://www.linkedin.com/in/hshaw215/'>LinkedIn</a>
+                <a class='menuItem' href='mailto:hu.shaw215@gmail.com'>Email</a>
+                <a class='menuItem' href='https://youtu.be/EvpbJDMiep4'>Tech Talk</a>
             </div>
-            
         </div>
         </>
     )
