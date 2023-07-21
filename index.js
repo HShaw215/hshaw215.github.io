e04eb08707454b4d2df1515ee58d95a8c8bd455e
HShaw215 <hu.shaw215@gmail.com>
2023-07-21 16:12:45 -0400

    removed unnecessary files

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './frontend/components/App.jsx';
 
 render (
