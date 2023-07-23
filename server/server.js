50563dff681af88c0b6637a1d070ee47e8ced5e6
HShaw215 <hu.shaw215@gmail.com>
2023-07-22 21:27:46 -0400

    added psuedocode to server

--- server/server.js
+++ server/server.js
@@ -3,13 +3,16 @@ import path from 'path';
 
 const app = express();
 
+//if production mode is running for deployment, serve build folder
 if (process.env.NODE_ENV === 'production'){
     app.use('/', express.static(path.resolve(__dirname, '../build')));
 }
 
+//global error handler
 app.use((err, req, res, next) => {
     console.log(err);
     res.status(500).send({error: err})
 })
 
+//start server on port 3000
 app.listen(3000, () => console.log('Server Listening on Port 3000'))
